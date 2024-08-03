2466e24a0b73541173fa2a94e0224a485505726f
W XC <wangxingchen12345678@gmail.com>
2024-08-03 21:47:44 +0800

    sup

--- login.js
+++ login.js
@@ -1,31 +1,5 @@
-function login_req() {
-    const form = document.getElementById('login');
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevent the default form submission
+let sign_up = document.getElementById("sign-up")
 
-        // Get form data
-        const formData = new FormData(form);
-        const username = formData.get('name');
-        const password = formData.get('pass');
+print(sign_up)
 
-        // Display form data
-        if (username == "123" && password == "321") {
-            window.location.href = "signup.html"
-        }
-        else {
-            alert("Incorrect username or password")
-        }
-        
-
-        // Optionally, do something with the data (e.g., send it to a server)
-        // fetch('your-server-endpoint', {
-        //     method: 'POST',
-        //     body: formData
-        // }).then(response => response.json())
-        //   .then(data => console.log(data))
-        //   .catch(error => console.error('Error:', error));
-    });
-}
-
-document.addEventListener("load", login_req());
